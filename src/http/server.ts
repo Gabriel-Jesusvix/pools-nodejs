c4e62d16d16bc7615394e2bfda055a7b7175607e
Gabriel Jesus <gabriel.developer2@gmail.com>
2024-02-12 20:52:04 -0300

    feat: websocket and ranking votes with redis

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,10 @@
 import cookie from '@fastify/cookie'
+import websocket from "@fastify/websocket"
 import { fastify } from 'fastify'
 import { createPoll } from './routes/create-poll'
 import { getPoll } from './routes/get-poll'
 import { voteOnPoll } from './routes/vote-on-poll'
+import { pollResults } from './ws/poll-results'
 
 const app = fastify()
 
@@ -10,6 +12,8 @@ app.register(cookie, {
   secret: "poll-application", 
   hook: 'onRequest', 
 })
+app.register(pollResults)
+app.register(websocket)
 app.register(createPoll)
 app.register(getPoll)
 app.register(voteOnPoll)
